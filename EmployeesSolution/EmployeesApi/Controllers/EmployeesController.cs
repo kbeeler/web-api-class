dc97403cf5abf49e3b6d899d9fc054320d7bf050
Kevin Beeler <kbeeler07@gmail.com>
2023-09-11 15:33:45 -0400

    before jeffs changes

---
+++ EmployeesSolution/EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+/*using EmployeesApi.Models;*/
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace EmployeesApi.Controllers;
+
+public class EmployeesController : ControllerBase
+{
+    [HttpGet("/employees")]
+    public async Task<ActionResult<EmployeeSummaryListResponse>> GetAllEmployees()
+    {
+        return Ok();
+    }
+
+    [HttpGet("/employees/{id}")]
+    public async Task<ActionResult<EmployeeDetailsItemResponse>> GetAnEmployee(string id)
+    {
+        return Ok();
+    }
+}
